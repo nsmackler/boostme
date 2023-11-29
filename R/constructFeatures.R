e0de93de4ce2b02f58841f0f3b290c52f44cec31
Noah Snyder-Mackler <nsmack@gmail.com>
2023-11-29 13:31:42 -0700

    Update constructFeatures.R
    fixed class issue that broke it

--- R/constructFeatures.R
+++ R/constructFeatures.R
@@ -50,7 +50,7 @@ constructFeatures <- function(bs,
     # need to make a copy of the data 'cause bsseq doesn't allow NAs
     otherMeths <- getMeth(bs[, -sample], type = "raw")
     otherCovs <- getCoverage(bs[, -sample], type = "Cov")
-    if (class(otherMeths) == "DelayedMatrix") {
+    if (class(otherMeths)[1] == "DelayedMatrix") {
       otherMeths <- as.matrix(otherMeths)
       otherCovs <- as.matrix(otherCovs)
     }
