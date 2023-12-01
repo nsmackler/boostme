5825e2e776823ce6de2fe1433d3d239eb0f1ee6a
Noah Snyder-Mackler <nsmack@gmail.com>
2023-11-30 21:08:09 -0700

    Update boostme.R
    fixed typo

--- R/boostme.R
+++ R/boostme.R
@@ -309,8 +309,8 @@ i=sample_index
       imputed <- newY
     }
     
-     write.table(metrics, file = paste0(colnames(dap_filtered_p1)[sample_index], "metrics.txt"), quote = F, sep = "\t", row.names = F, col.names = F)
-     write(imputed, file = paste0(colnames(dap_filtered_p1)[sample_index], "imputed.txt"), ncolumns=1)
+     write.table(metrics, file = paste0(colnames(bs)[sample_index], "metrics.txt"), quote = F, sep = "\t", row.names = F, col.names = F)
+     write(imputed, file = paste0(colnames(bs)[sample_index], "imputed.txt"), ncolumns=1)
   if (verbose) {
       message(paste(Sys.time(), "... Saved results to", save))
     }
